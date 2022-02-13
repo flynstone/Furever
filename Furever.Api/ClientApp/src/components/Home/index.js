28f1bddf48afd15f3110f606a44d75b406c7d8ac
Neavy18 <genevieve.gt18@gmail.com>
2022-02-13 13:58:37 -0500

    Add basic homepage layout with animal tiles on page

--- Furever.Api/ClientApp/src/components/Home/index.js
+++ Furever.Api/ClientApp/src/components/Home/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Animal from '../Animal';
 import AnimalPopUp from '../AnimalPopUp';
+import "./Home.css"
+
 
 const ANIMALS = [
   {
@@ -8,8 +10,48 @@ const ANIMALS = [
     name: 'Animal #1',
     age: '3',
     breed: 'cat'
-  }
+  },
+  {
+    IMG: 'IMAGE',
+    name: 'Animal #2',
+    age: '6',
+    breed: 'dog'
+  },
   
+  {
+    IMG: 'IMAGE',
+    name: 'Animal #3',
+    age: '7',
+    breed: 'dog'
+  },
+
+  {
+    IMG: 'IMAGE',
+    name: 'Animal #4',
+    age: '1',
+    breed: 'cat'
+  },
+
+  {
+    IMG: 'IMAGE',
+    name: 'Animal #5',
+    age: '10',
+    breed: 'cat'
+  },
+
+  {
+    IMG: 'IMAGE',
+    name: 'Animal #6',
+    age: '8',
+    breed: 'dog'
+  },
+
+  {
+    IMG: 'IMAGE',
+    name: 'Animal #7',
+    age: '12',
+    breed: 'dog'
+  }
 
 ]
 
@@ -18,10 +60,12 @@ const Home = ({ name }) => {
 
   return (
     <div>
-      <h1>Find your Soulmate</h1>
-      {ANIMALS.map(animal => (
-        <Animal animal={animal} setSelectedAnimal={setSelectedAnimal} />
-      ))}
+      <h1 className='Slogan'>Find your Soulmate</h1>
+      <div className='AnimalTiles'>
+        {ANIMALS.map(animal => (
+          <Animal animal={animal} setSelectedAnimal={setSelectedAnimal} />
+        ))}
+      </div>
       {/* {selectedAnimal !== null (
         <AnimalPopUp animal={selectedAnimal} setSelectedAnimal={setSelectedAnimal} />
       )} */}
