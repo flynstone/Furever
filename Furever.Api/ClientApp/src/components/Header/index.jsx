cf7ebeba6669098884bf383171cdab405401c790
Neavy18 <genevieve.gt18@gmail.com>
2022-02-13 13:07:49 -0500

    Add basic css for Header

--- Furever.Api/ClientApp/src/components/Header/index.jsx
+++ Furever.Api/ClientApp/src/components/Header/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>
+    <div className='Header'>
       <div>Profile</div> 
       <div>Furever Home</div>
       <div>Extra parameters</div>
