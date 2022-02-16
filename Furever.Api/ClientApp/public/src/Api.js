dc0a9991a72e5b7797234428187e1200b821179d
Julien <flynstone@live.ca>
2022-02-16 01:05:38 -0500

    commit git ignore fixes

--- Furever.Api/ClientApp/public/src/Api.js
+++ Furever.Api/ClientApp/public/src/Api.js
@@ -2,30 +2,30 @@ import axios from 'axios';
 import React from 'react';
 import { useState, useEffect } from React;
 
-export default function setInfoData () {
+export default function setInfoData() {
 
-  const [animalInfo, setAnimalInfo] = useState({
-    animal: {},
-    refuge: {}
-  });
+    const [animalInfo, setAnimalInfo] = useState({
+        animal: {},
+        refuge: {}
+    });
 
-  const [profile, setProfile] = useState({
-    profile: {}
-  })
-
-  useEffect(() => {
-    const apiAnimals = "/api/animals"
-    const apiRefuges = "/api/refuges"
-    const apiProfile = "/api/profile"
-  
-    Promise.all([
-      axios.get(apiAnimals),
-      axios.get(apiRefuges),
-      axios.get(apiProfile)
-    ]).then((all) => {
-      console.log("this is from me the api calls!!! ----->", all)
-      setState(prev => ({}))
+    const [profile, setProfile] = useState({
+        profile: {}
     })
-  }, []);
-  
+
+    useEffect(() => {
+        const apiAnimals = "/api/animals"
+        const apiRefuges = "/api/refuges"
+        const apiProfile = "/api/profile"
+
+        Promise.all([
+            axios.get(apiAnimals),
+            axios.get(apiRefuges),
+            axios.get(apiProfile)
+        ]).then((all) => {
+            console.log("this is from me the api calls!!! ----->", all)
+            setState(prev => ({}))
+        })
+    }, []);
+
 }
---
+++ Furever.Identity/Models/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Furever.Identity.Models.Configurations;
+
+namespace Furever.Identity.Models
+{
+    public class UserContext : IdentityDbContext<User>
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new RoleConfiguration());
+        }
+    }
+}
